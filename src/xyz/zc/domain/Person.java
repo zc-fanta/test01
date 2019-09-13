b0b42cf9fcea86b01356cf5d9b00c305dcafd0a5
chen <jc.ustb@163.com>
2019-09-13 10:13:27 +0800

    1

--- src/xyz/zc/domain/Person.java
+++ src/xyz/zc/domain/Person.java
@@ -7,7 +7,7 @@ package xyz.zc.domain;
 public class Person {
     private String id;
     private String name;
-    private String address;
-    
     private String phone;
+
+    private String address;
 }
